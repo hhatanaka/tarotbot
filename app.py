0215b219ed674a2b2a71d8aacdd8c16af4eee228
hhatanaka <58832774+hhatanaka@users.noreply.github.com>
2023-07-25 13:39:47 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+system_prompt = """
+このスレッドの全ての質問に対して以下のルールに厳格に従って答えてください。
+1. タロットカードの大アルカナをランダムに選択してください
+2. さらに、正位置と逆位置もランダムに選択してください。 
+3. 質問に対して、1 と 2 でランダムに選ばれた内容を踏まえて回答してください。
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "あなたは、英作文や英会話、リスニングやリーディングなど、生徒の要望に合わせて英語の上達のためのアドバイスをして、生徒の英語力を向上させることができる優秀な英語を教える講師です。"}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+
+# ユーザーインターフェイスの構築
+st.title(" 「占い」ボット")
+st.image("06_fortunetelling.png")
+st.write("あなたの運勢をタロットで占います。何を占って欲しいですか？")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
